f4b63f80905fbd0019049c9f0ffcfd17f2a07ae2
Andrew Craft <drewtcraft@gmail.com>
2023-05-31 15:44:31 -0400

    add build script for building scss files

---
+++ build.rs
@@ -0,0 +1,39 @@
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process::Command;
+use walkdir::WalkDir;
+
+fn main() {
+    let mut output_file = File::create("src/concat.scss").expect("failed to create output file");
+
+    // Walk through src directory and search for SCSS files
+    for entry in WalkDir::new("src") {
+        let entry = entry.expect("failed to read directory entry");
+
+        if entry.path().extension().map_or(false, |e| e == "scss") {
+            let contents = fs::read_to_string(entry.path()).expect("failed to read scss file");
+
+            // Write to the output file
+            output_file
+                .write_all(contents.as_bytes())
+                .expect("failed to write to output file");
+
+            output_file
+                .write_all(b"\n")
+                .expect("failed to write newline to output file");
+        }
+    }
+
+    // Execute the sass command
+    let output = Command::new("sass")
+        .args(&["src/concat.scss", "public/css/style.css"])
+        .output()
+        .expect("failed to execute sass");
+
+    fs::remove_file("src/concat.scss").expect("failed to remove concat.scss");
+
+    // Print sass's stdout and stderr
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+    eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+}
--- src/router.rs
+++ src/router.rs
@@ -1,16 +1,23 @@
 use axum::{
     http::StatusCode,
     response::{Html, IntoResponse},
+    routing::get_service,
     Router,
 };
+use tower_http::services::ServeDir;
 
 use crate::apps::user;
 
 pub fn get_routes() -> Router {
     Router::new()
         .merge(user::routes::routes())
-        // .layer(user::layers::authorized)
+        .merge(static_routes())
         .fallback(page_not_found)
+}
+
+fn static_routes() -> Router {
+    // ex. "BASE_URL/public/js/htmx.min.js"
+    Router::new().nest_service("/public", get_service(ServeDir::new("./public")))
 }
 
 async fn page_not_found() -> impl IntoResponse {
